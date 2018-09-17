ac210e660a942d1cd6413c757a4958eb7a44b7be
GodLesZ <GodLesZ@users.noreply.github.com>
2018-09-17 07:53:35 +0200

    Try working on browser support

--- index.js
+++ index.js
@@ -1,181 +1,180 @@
-var pseudoRandom = Math.random;
+(function () {
 
-var visaPrefixList = new Array(
-    "4539",
-    "4556",
-    "4916",
-    "4532",
-    "4929",
-    "40240071",
-    "4485",
-    "4716",
-    "4"
-);
+    var pseudoRandom = Math.random;
 
-var mastercardPrefixList = new Array(
-    "51",
-    "52",
-    "53",
-    "54",
-    "55"
-);
+    var visaPrefixList = new Array(
+        "4539",
+        "4556",
+        "4916",
+        "4532",
+        "4929",
+        "40240071",
+        "4485",
+        "4716",
+        "4"
+    );
 
-var amexPrefixList = new Array(
-    "34",
-    "37"
-);
+    var mastercardPrefixList = new Array(
+        "51",
+        "52",
+        "53",
+        "54",
+        "55"
+    );
 
-var discoverPrefixList = new Array("6011");
+    var amexPrefixList = new Array(
+        "34",
+        "37"
+    );
 
-var dinersPrefixList = new Array(
-    "300",
-    "301",
-    "302",
-    "303",
-    "36",
-    "38"
-);
+    var discoverPrefixList = new Array("6011");
 
-var enRoutePrefixList = new Array(
-    "2014",
-    "2149"
-);
+    var dinersPrefixList = new Array(
+        "300",
+        "301",
+        "302",
+        "303",
+        "36",
+        "38"
+    );
 
-var jcbPrefixList = new Array(
-    "35"
-);
+    var enRoutePrefixList = new Array(
+        "2014",
+        "2149"
+    );
 
-var voyagerPrefixList = new Array("8699");
+    var jcbPrefixList = new Array(
+        "35"
+    );
 
-var schemes = {
-    "VISA": {
-        prefixList: visaPrefixList,
-        digitCount: 16
-    },
-    "MasterCard": {
-        prefixList: mastercardPrefixList,
-        digitCount: 16
-    },
-    "Amex": {
-        prefixList: amexPrefixList,
-        digitCount: 15
-    },
-    "Diners": {
-        prefixList: dinersPrefixList,
-        digitCount: 16
-    },
-    "Discover": {
-        prefixList: discoverPrefixList,
-        digitCount: 16
-    },
-    "EnRoute": {
-        prefixList: enRoutePrefixList,
-        digitCount: 16
-    },
-    "JCB": {
-        prefixList: jcbPrefixList,
-        digitCount: 16
-    },
-    "Voyager": {
-        prefixList: voyagerPrefixList,
-        digitCount: 16
-    }
-};
+    var voyagerPrefixList = new Array("8699");
 
-/**
- * Revert a String
- * @param  {String} str
- * @return {String}
- */
-function strrev(str) {
-   if (!str) return '';
-   var revstr='';
-   for (var i = str.length-1; i>=0; i--)
-       revstr+=str.charAt(i)
-   return revstr;
-}
+    /**
+     * Supported Card Schemes
+     * @type {object}
+     */
+    var schemes = {
+        "VISA": {
+            prefixList: visaPrefixList,
+            digitCount: 16
+        },
+        "MasterCard": {
+            prefixList: mastercardPrefixList,
+            digitCount: 16
+        },
+        "Amex": {
+            prefixList: amexPrefixList,
+            digitCount: 15
+        },
+        "Diners": {
+            prefixList: dinersPrefixList,
+            digitCount: 16
+        },
+        "Discover": {
+            prefixList: discoverPrefixList,
+            digitCount: 16
+        },
+        "EnRoute": {
+            prefixList: enRoutePrefixList,
+            digitCount: 16
+        },
+        "JCB": {
+            prefixList: jcbPrefixList,
+            digitCount: 16
+        },
+        "Voyager": {
+            prefixList: voyagerPrefixList,
+            digitCount: 16
+        }
+    };
 
-/**
- * Complete a prefixed number-string
- * @param  {String} prefix  is the start of the CC number as a string, any number of digits
- * @param  {Number} length  is the length of the CC number to generate. Typically 13 or 16
- * @return {String}
- */
-function completed_number(prefix, length) {
-
-    var ccnumber = prefix;
-
-    // generate digits
-
-    while ( ccnumber.length < (length - 1) ) {
-        ccnumber += Math.floor(pseudoRandom()*10);
+    /**
+     * Revert a String
+     * @param  {String} str
+     * @return {String}
+     */
+    function strrev(str) {
+        if (!str) return '';
+        var revstr='';
+        for (var i = str.length-1; i>=0; i--)
+            revstr+=str.charAt(i)
+        return revstr;
     }
 
-    // reverse number and convert to int
+    /**
+     * Complete a prefixed number-string
+     * @param  {String} prefix  is the start of the CC number as a string, any number of digits
+     * @param  {Number} length  is the length of the CC number to generate. Typically 13 or 16
+     * @return {String}
+     */
+    function completed_number(prefix, length) {
 
-    var reversedCCnumberString = strrev( ccnumber );
+        var ccnumber = prefix;
 
-    var reversedCCnumber = new Array();
-    for ( var i=0; i < reversedCCnumberString.length; i++ ) {
-        reversedCCnumber[i] = parseInt( reversedCCnumberString.charAt(i) );
-    }
+        // generate digits
 
-    // calculate sum
-
-    var sum = 0;
-    var pos = 0;
-
-    while ( pos < length - 1 ) {
-
-        var odd = reversedCCnumber[ pos ] * 2;
-        if ( odd > 9 ) {
-            odd -= 9;
+        while ( ccnumber.length < (length - 1) ) {
+            ccnumber += Math.floor(pseudoRandom()*10);
         }
 
-        sum += odd;
+        // reverse number and convert to int
 
-        if ( pos != (length - 2) ) {
+        var reversedCCnumberString = strrev( ccnumber );
 
-            sum += reversedCCnumber[ pos +1 ];
+        var reversedCCnumber = new Array();
+        for ( var i=0; i < reversedCCnumberString.length; i++ ) {
+            reversedCCnumber[i] = parseInt( reversedCCnumberString.charAt(i) );
         }
-        pos += 2;
+
+        // calculate sum
+
+        var sum = 0;
+        var pos = 0;
+
+        while ( pos < length - 1 ) {
+
+            var odd = reversedCCnumber[ pos ] * 2;
+            if ( odd > 9 ) {
+                odd -= 9;
+            }
+
+            sum += odd;
+
+            if ( pos != (length - 2) ) {
+
+                sum += reversedCCnumber[ pos +1 ];
+            }
+            pos += 2;
+        }
+
+        // calculate check digit
+
+        var checkdigit = (( Math.floor(sum/10) + 1) * 10 - sum) % 10;
+        ccnumber += checkdigit;
+
+        return ccnumber;
+
     }
 
-    // calculate check digit
+    /**
+     * Actually generate a credit card number
+     * @param  {[type]} prefixList [description]
+     * @param  {[type]} length     [description]
+     * @param  {[type]} howMany    [description]
+     * @return {[type]}            [description]
+     */
+    function credit_card_number(prefixList, length, howMany) {
 
-    var checkdigit = (( Math.floor(sum/10) + 1) * 10 - sum) % 10;
-    ccnumber += checkdigit;
+        var result = new Array();
+        for (var i = 0; i < howMany; i++) {
 
-    return ccnumber;
+            var randomArrayIndex = Math.floor(pseudoRandom() * prefixList.length);
+            var ccnumber = prefixList[ randomArrayIndex ];
+            result.push( completed_number(ccnumber, length) );
+        }
 
-}
-
-/**
- * Actually generate a credit card number
- * @param  {[type]} prefixList [description]
- * @param  {[type]} length     [description]
- * @param  {[type]} howMany    [description]
- * @return {[type]}            [description]
- */
-function credit_card_number(prefixList, length, howMany) {
-
-    var result = new Array();
-    for (var i = 0; i < howMany; i++) {
-
-        var randomArrayIndex = Math.floor(pseudoRandom() * prefixList.length);
-        var ccnumber = prefixList[ randomArrayIndex ];
-        result.push( completed_number(ccnumber, length) );
+        return result;
     }
-
-    return result;
-}
-
-/**
- * Supported Card Schemes
- * @type {Array}
- */
-module.exports = {
-    Schemes: schemes,
 
     /**
      * The entry-point function
@@ -184,15 +183,25 @@ module.exports = {
      * @param {Number} [randomGen] Pseudo Random Generator. Must generate a random number between 0 an 1
      * @return {String}
      */
-    GenCC: function(CardScheme, howMany, randomGen){
+    function generateCreditCard(CardScheme, howMany, randomGen){
         pseudoRandom = randomGen || pseudoRandom;
         var amount = howMany || 1;
-        
+
         // Defaults to MasterCard
         if (!schemes[CardScheme]) {
             CardScheme = "MasterCard";
         }
-        
+
         return credit_card_number(schemes[CardScheme].prefixList, schemes[CardScheme].digitCount, amount);
     }
-};
+
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = {
+            Schemes: schemes,
+            GenCC: generateCreditCard,
+        };
+    } else {
+        window.CCSchemes = schemes;
+        window.GenCC = generateCreditCard;
+    }
+})();
